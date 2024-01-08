049e30c936f06be4314d215ca0df507217763feb
Antoni0af22 <62048162+Antoni0af22@users.noreply.github.com>
2024-01-08 05:48:48 -0600

    Add files via upload

---
+++ app/borrar.js
@@ -0,0 +1,25 @@
+import {onGetLibros} from './firebase.js'
+
+const librosContainer = document.getElementById('libros-container')
+
+window.addEventListener('DOMContentLoaded',async () =>{
+
+    //const querySnapshots = await getLibros();
+    onGetLibros((querySnapshot)=>{
+        librosContainer.innerHTML = ''
+
+        querySnapshot.forEach((doc) => {        
+            const libro = doc.data()
+            librosContainer.innerHTML += `
+            <div class="card card-body mt-2 border-primary">
+            <h3 class="h5">${libro.titulo}</h3>
+            <p>${libro.autor}</p>
+            <p>${libro.editorial}</p>
+            <p>${libro.isbn}</p>
+            <p>${libro.fecha}</p>
+            <div>`;
+        });
+
+        librosContainer.innerHTML = html;
+    })
+})
