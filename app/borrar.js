e9b1a8f471d7044ce44d76e1b061b6a2bd4fa923
Antoni0af22 <62048162+Antoni0af22@users.noreply.github.com>
2024-01-08 06:31:31 -0600

    Update borrar.js

--- app/borrar.js
+++ app/borrar.js
@@ -1,16 +1,13 @@
 import {onGetLibros} from './firebase.js'
 
-const librosContainer = document.getElementById('libros-container')
+const librosContainer = document.getElementById('libros-container');
 
-window.addEventListener('DOMContentLoaded',async () =>{
-
-    //const querySnapshots = await getLibros();
+window.addEventListener("DOMContentLoaded", async (e) => {
     onGetLibros((querySnapshot)=>{
-        librosContainer.innerHTML = '';
-
+        let html = '';
         querySnapshot.forEach((doc) => {        
-            const libro = doc.data()
-            librosContainer.innerHTML += `
+            const libro = doc.data();
+            html += `
             <div class="card card-body mt-2 border-primary">
             <h3 class="h5">${libro.titulo}</h3>
             <p>${libro.autor}</p>
